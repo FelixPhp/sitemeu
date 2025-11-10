491fa4fb18240ec8f58283369b987e0d1c0a42d9
Felix Pessoa <132860859+FelixPhp@users.noreply.github.com>
2025-11-10 15:17:45 -0400

    tes css

--- relatorios.php
+++ relatorios.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Relatórios</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="css/style.css">
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head><body>
 <div class="container-app">
